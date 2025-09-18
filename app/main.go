17782aec3fe86303d6e2999f5e3256d86aa4a32a
ArtemKVD <a.kunicza@list.ru>
2025-09-19 00:21:26 +0300

    exit command

---
+++ app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
+var _ = fmt.Fprint
+
+func main() {
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Println("Reader error")
+		}
+		if strings.TrimSpace(command) == "exit 0" {
+			os.Exit(0)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found") //
+	}
+}
