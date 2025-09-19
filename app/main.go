850bda4bb140bd523d3d7ce03c9acca9298037f9
ArtemKVD <a.kunicza@list.ru>
2025-09-19 23:30:22 +0300

    some funcs

--- app/main.go
+++ app/main.go
@@ -19,8 +19,22 @@ func main() {
 			log.Println("Reader error")
 		}
 		if strings.TrimSpace(command) == "exit 0" {
+			fmt.Println(command)
 			os.Exit(0)
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found") //
+		if strings.Contains(command, "echo") {
+			fmt.Println(command[4:])
+		}
+
+		if command[:4] == "type" {
+			if strings.Contains(command, "echo") {
+				fmt.Printf("echo is a shell builtin")
+			}
+			if strings.Contains(command, "exit") {
+				fmt.Printf("exit is a shell builtin")
+			}
+
+		}
+		//fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
