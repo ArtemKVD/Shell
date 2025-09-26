4930b23e49e2fc01e8252bd3edb3cdffbd35d7b7
ArtemKVD <a.kunicza@list.ru>
2025-09-26 23:06:19 +0300

    Gitignore, configuration reload (9), environment variables(8)

--- app/main.go
+++ app/main.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+
+	"github.com/joho/godotenv"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -18,6 +22,16 @@ func main() {
 	}
 	defer file.Close()
 
+	SigChan := make(chan os.Signal, 1)
+	signal.Notify(SigChan, syscall.SIGHUP)
+
+	go func() {
+		for {
+			<-SigChan
+			fmt.Println("Configuration reload")
+		}
+	}()
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -27,6 +41,12 @@ func main() {
 		_, errf := file.WriteString(command)
 		if errf != nil {
 			fmt.Println("History write error")
+		}
+
+		if strings.TrimSpace(command) == "test sighup" {
+			process, _ := os.FindProcess(os.Getpid())
+			process.Signal(syscall.SIGHUP)
+			continue
 		}
 
 		if strings.TrimSpace(command) == "exit 0" {
@@ -41,6 +61,22 @@ func main() {
 			fmt.Println(command[4:])
 		}
 
+		if strings.Contains(command, "/e $") {
+			err := godotenv.Load()
+			if err != nil {
+				fmt.Println("godotenv load error")
+			}
+			if strings.Contains(command, ":") {
+				envs := os.Environ()
+				for _, e := range envs {
+					fmt.Println(e)
+				}
+			} else {
+				path := strings.TrimSpace(command[4:])
+				fmt.Println(os.Getenv(path))
+			}
+		}
+
 		if len(command) >= 4 && command[:4] == "type" {
 			if strings.Contains(command, "echo") {
 				fmt.Println("echo is a shell builtin")
@@ -53,6 +89,7 @@ func main() {
 			fmt.Println("Ctrl+D")
 			os.Exit(0)
 		}
+
 		//fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
